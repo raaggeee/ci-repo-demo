169e05bbaf80ca719217fcfeda2f0f776f41dd6c
raaggeee <raaggee08@gmail.com>
2026-03-21 14:23:03 +0530

    debug app:

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-def add(a, b): return a + b
+def add(a, b): 
+    return a + b
 
-def sub(a=10, b=0):
-    return a -     b 
+def sub(a, b):
+    return a - b 
 
 
 print(add(10, 20))
-print(sub())
+print(sub(10, 20))
